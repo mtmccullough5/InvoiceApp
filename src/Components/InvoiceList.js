f476dead99b40cc91f4df0c79d2e835947ec6ba7
Matthew McCullough <mcculloughmatthew5@gmail.com>
2018-10-29 21:06:14 -0700

    Basic Component Structure

---
+++ src/Components/InvoiceList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InvoiceList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default InvoiceList
---
+++ src/Components/InvoiceTotals.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InvoiceTotals extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default InvoiceTotals
